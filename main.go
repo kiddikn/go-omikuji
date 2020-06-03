28de39e0d81723e57ee4f0ecb12ceeafcdcda2eb
kiddikn <kiddikn@gmail.com>
2020-06-03 22:40:39 +0900

    返却値をjson形式に変更

--- main.go
+++ main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"time"
 )
+
+// Omikuji おみくじjson
+type Omikuji struct {
+	Omikuji string `json:"omikuji"`
+}
 
 var omikuzi = map[int]string{0: "大吉", 1: "中吉", 2: "末吉", 3: "凶"}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	item := rand.Intn(len(omikuzi))
 	result, _ := omikuzi[item]
-	fmt.Fprint(w, result)
+	omikujijson := &Omikuji{Omikuji: result}
+
+	var buf bytes.Buffer
+	enc := json.NewEncoder(&buf)
+	if err := enc.Encode(omikujijson); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprint(w, buf.String())
 }
 
 func main() {
